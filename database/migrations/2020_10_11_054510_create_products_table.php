86668ee21615f8d135b05fcaec5cb118dbec352f
zivkoobradovic <37649807+zivkoo@users.noreply.github.com>
2020-10-16 09:19:14 +0200

    Store Created

--- database/migrations/2020_10_11_054510_create_products_table.php
+++ database/migrations/2020_10_11_054510_create_products_table.php
@@ -15,6 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('store');
             $table->integer('sku');
             $table->string('name');
             $table->integer('price');
