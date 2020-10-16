2c8cdaf88273b359f03d4488327bcee129763432
zivkoobradovic <37649807+zivkoo@users.noreply.github.com>
2020-10-16 10:32:22 +0200

    Update ProductFactory.php

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Store;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -22,7 +23,15 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'store' => Store::factory(),
+            'sku' => $this->faker->randomNumber(10),
+            'name' => $this->faker->words(2),
+            'price'=> $this->numberBetween($min = 100, $max = 30000),
+            'short_description' => $this->faker->paragraph,
+            'long_description' => $this->faker->paragraphs,
+            'in_stock' => $this->faker->boolean,
+            'badge' => 'new',
+            'main_image' => $this->faker->image('https://source.unsplash.com/collection/4814656/',640,480, null, false),
         ];
     }
 }
